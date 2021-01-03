fa3734d9650aaa1429a88b9f14b28934d9c7c550
Reghill J4 <rj4@lenovo.com>
2021-01-03 16:50:55 +0530

    first commit

---
+++ src/main/java/Processing.java
@@ -0,0 +1,32 @@
+import processing.core.PApplet;
+
+public class Processing extends PApplet {
+
+    public static final int WIDTH = 720;
+    public static final int HEIGHT = 640;
+    public static final int DIAMETER = 20;
+    int xOffset = 0;
+
+    public static void main(String[] args) {
+        PApplet.main("Processing", args);
+    }
+
+    @Override
+    public void settings() {
+        super.settings();
+        size(WIDTH, HEIGHT);
+    }
+
+    @Override
+    public void draw() {
+        for (int position = 1; position <= 4; position++) {
+            drawCircle(position);
+        }
+        xOffset++;
+    }
+
+    private void drawCircle(int position) {
+        int yOffset = (HEIGHT * position) / 5;
+        ellipse(xOffset * position, yOffset, DIAMETER, DIAMETER);
+    }
+}
